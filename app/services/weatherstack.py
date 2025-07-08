43fde031e83e0b6ef1ed805fe9c43d474cae8a27
Yoyo-su <iohannan211@gmail.com>
2025-07-08 11:43:17 +0100

    create weatherstack service

---
+++ app/services/weatherstack.py
@@ -0,0 +1,80 @@
+import requests
+from dotenv import load_dotenv
+import os
+from .cache import get_cache_key, check_cache, cache_response
+
+# Load env variables
+load_dotenv()
+
+as_api_key = os.getenv("WEATHERSTACK_API_KEY")
+
+
+def get_current_weather_info(latitude: str = None, longtitude: str = None, as_api_key: str = as_api_key):
+    """This function retrieves weather information from the AviationStack API based
+    on the provided latitude and longitude, and returns the current weather data.
+
+    Args:
+        latitude (str, optional): Latitude of the airport. Defaults to None.
+        longtitude (str, optional): Longitude of the airport. Defaults to None.
+        as_api_key (str, optional): WeatherStack API key. Defaults to the value from environment variables.
+
+    Raises:
+        ValueError: - If the API key is missing.
+                    - If the location is not found or if multiple results are returned.
+                    - If the latitude or longitude is not provided.
+                    - If there is any other validation error with the input parameters.
+        RequestException: If there is an error with the request to the WeatherStack API.
+        Exception: If any unexpected errors during the execution.
+
+    Returns:
+        dict: A dictionary containing weather information if the request is successful.
+    """
+    try:
+        # Validate variables
+        if not as_api_key:
+            raise ValueError(
+                "WEATHERSTACK_API_KEY is not set in the environment variables"
+            )
+        if not latitude or not longtitude:
+            raise ValueError("Latitude and longitude must be provided")
+
+        # Construct the query
+        query = f"query={latitude},{longtitude}"
+        # Construct the URL with the query
+        url = (
+            f"https://api.weatherstack.com/current?access_key={as_api_key}&{query}"
+        )
+        cache_key = get_cache_key(url)
+        if cache_key:
+            # Check if the data is already cached
+            cache_data = check_cache(cache_key)
+
+        if cache_data:
+            print("Using cached data...")
+            airport_info = cache_data
+        else:
+            # Make the API request
+            print("Making API request...")
+            response = requests.get(url)
+            weather_info = response.json()
+            cache_response(cache_key, weather_info)
+
+        # If the response does not contain exactly one location, raise an error
+        # if len(weather_info["current"]) != 1:
+        #     raise ValueError(
+        #         f"Weather information for latitude {latitude} and longitude {longtitude} not found or multiple results returned"
+        #     )
+
+        # If the response is valid, return the weather information
+        print(f"weather info for {latitude}, {longtitude} retrieved successfully")
+        return weather_info
+
+    except requests.exceptions.RequestException as e:
+        print(f"Request error: {e}")
+        raise e
+    except ValueError as e:
+        print(f"Value error: {e}")
+        raise e
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        raise e
